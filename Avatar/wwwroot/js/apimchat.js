17affa6f74d839b942ae3e054101f20125ccc00f
Kyle Raymond <117292869+skraymo1@users.noreply.github.com>
2024-09-19 09:24:45 -0500

    Update apimchat.js

--- Avatar/wwwroot/js/apimchat.js
+++ Avatar/wwwroot/js/apimchat.js
@@ -17,7 +17,6 @@ var spokenTextQueue = []
 var sessionActive = false
 var lastSpeakTime
 var imgUrl = ""
-//var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImV4cCI6MTcyNjM1NDMyOX0._3BRuuv4CVH_yDtJJtPdglI8zXpK_cdaYODWKNHMQes"
 // Connect to avatar service
 function connectAvatar() {
     const cogSvcRegion = document.getElementById('region').value
@@ -57,13 +56,10 @@ function connectAvatar() {
 
         console.log("Event received: " + e.description + offsetMessage)
     }
-    //wss://westus2.stt.speech.microsoft.com/speech/universal/v2?language=en-US&format=simple&lidEnabled=true&Ocp-Apim-Subscription-Key=5055a233fbe54e64a61250fdc470df70&X-ConnectionId=F2BD2C16B10E4D2D80009AC8F9BF3CDD
-
+  
     const speechRecognitionConfig = SpeechSDK.SpeechConfig.fromEndpoint(new URL(`wss://${cogSvcRegion}.stt.speech.microsoft.com/speech/universal/v2`), cogSvcSubKey)
     //const speechRecognitionConfig = SpeechSDK.SpeechConfig.fromEndpoint(new URL(`wss://{{APIM SOCKET ENDPOINT}`), cogSvcSubKey)
     //speechRecognitionConfig.authorizationToken=token
-        //`wss://${cogSvcRegion}.stt.speech.microsoft.com/speech/universal/v2`), "5055a233fbe54e64a61250fdc470df70")//cogSvcSubKey)
-    //const speechRecognitionConfig = SpeechSDK.SpeechConfig.fromEndpoint(new URL(`wss://avatarapimdev.azure-api.net/speechsocket`), cogSvcSubKey)
     speechRecognitionConfig.setProperty(SpeechSDK.PropertyId.SpeechServiceConnection_LanguageIdMode, "Continuous")
     var sttLocales = document.getElementById('sttLocales').value.split(',')
     var autoDetectSourceLanguageConfig = SpeechSDK.AutoDetectSourceLanguageConfig.fromLanguages(sttLocales)
@@ -106,7 +102,6 @@ function connectAvatar() {
         xhr.open("GET", `https://${cogSvcRegion}.tts.speech.microsoft.com/cognitiveservices/avatar/relay/token/v1`)
         //xhr.open("GET", `{{APIM SPEECH ENDPOINT}}`)
     }
-    //https://avatarapimdev.azure-api.net/speech/avatar
     //https://${cogSvcRegion}.tts.speech.microsoft.com/cognitiveservices/avatar/relay/token/v1
     
     xhr.setRequestHeader("Ocp-Apim-Subscription-Key", cogSvcSubKey)
@@ -418,7 +413,6 @@ function handleUserQuery(userQuery, userQueryHTML, imgUrlPath) {
     const azureOpenAIDeploymentName = document.getElementById('azureOpenAIDeploymentName').value
 
     let url = "{AOAIEndpoint}/openai/deployments/{AOAIDeployment}/chat/completions?api-version=2023-06-01-preview".replace("{AOAIEndpoint}", azureOpenAIEndpoint).replace("{AOAIDeployment}", azureOpenAIDeploymentName)
-    //let url = "{AOAIEndpoint}/deployments/{AOAIDeployment}/chat/completions?api-version=2023-06-01-preview".replace("{AOAIEndpoint}", azureOpenAIEndpoint).replace("{AOAIDeployment}", azureOpenAIDeploymentName)
     let body = JSON.stringify({
         messages: messages,
         stream: true
@@ -426,7 +420,6 @@ function handleUserQuery(userQuery, userQueryHTML, imgUrlPath) {
 
     if (dataSources.length > 0) {
         url = "{AOAIEndpoint}/openai/deployments/{AOAIDeployment}/extensions/chat/completions?api-version=2023-06-01-preview".replace("{AOAIEndpoint}", azureOpenAIEndpoint).replace("{AOAIDeployment}", azureOpenAIDeploymentName)
-        //url = "{AOAIEndpoint}/deployments/{AOAIDeployment}/extensions/chat/completions?api-version=2023-06-01-preview".replace("{AOAIEndpoint}", azureOpenAIEndpoint).replace("{AOAIDeployment}", azureOpenAIDeploymentName)
         body = JSON.stringify({
             dataSources: dataSources,
             messages: messages,
